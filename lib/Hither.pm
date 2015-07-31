de43745349c5fe78de2024dfb90d3af451805f26
Ingy döt Net <ingy@ingy.net>
2015-07-31 12:08:03 -0700

    Start of hsd-to-dbic support

--- lib/Hither.pm
+++ lib/Hither.pm
@@ -2,4 +2,16 @@ use strict; use warnings;
 package Hither;
 our $VERSION = '0.0.2';
 
+use YAML::XS;
+use Exporter 'import';
+
+our @EXPORT = qw(
+  read_input
+  type_to_field
+);
+
+sub read_input {
+  do {local $/; <>};
+}
+
 1;
