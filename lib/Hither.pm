cc27a7d353accc422639550911d2c5116bb49c26
Ingy döt Net <ingy@ingy.net>
2015-07-31 18:12:24 -0400

    Work on HSD-to-DBIC

--- lib/Hither.pm
+++ lib/Hither.pm
@@ -2,12 +2,14 @@ use strict; use warnings;
 package Hither;
 our $VERSION = '0.0.2';
 
-use YAML::XS;
+use YAML::XS qw(Load LoadFile);
 use Exporter 'import';
 
 our @EXPORT = qw(
   read_input
   type_to_field
+  Load
+  LoadFile
 );
 
 sub read_input {
